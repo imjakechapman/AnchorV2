c8e1cf24c75532c4195536539f57e0e4d74c2e87
Jake Chapman <jake@imjakechapman.com>
2014-05-06 17:48:14 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,32 @@
 // Gulp Requires
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    rjs = require('gulp-requirejs'),
+    clean = require('gulp-clean'),
+    imagemin = require('gulp-imagemin'),
     uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
     notify = require('gulp-notify'),
+    autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
     livereload = require('gulp-livereload'),
-    rjs = require('gulp-requirejs');
+    lr = require('tiny-lr'),
+    server = lr();
  
 // Node requires for exec and sys
 var exec = require('child_process').exec,
     sys = require('sys');
  
+
+
 // Directories
 var SRC = 'public/assets',
     DIST = 'public/dist';
  
+
+
 // SCSS Compiling and Minification
 gulp.task('scss', function(){
   return gulp.src(SRC + '/styles/app.scss')
@@ -33,11 +45,28 @@ gulp.task('scss', function(){
         }
       })
     )
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
     .pipe( gulp.dest(DIST + '/styles') )
-    .pipe(livereload());
+    .pipe(livereload(server));
 });
 
-// Build JS through R.js
+
+
+
+// Image Minification
+gulp.task('compress-images', function () {
+    return gulp.src( SRC + '/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest( DIST + '/images' ))
+        .pipe(notify('Images compressed'));
+});
+
+
+
+
+// JS Build with R.js
 gulp.task('requirejsBuild', function() {
   rjs({
       baseUrl: SRC + '/js/',
@@ -51,16 +80,40 @@ gulp.task('requirejsBuild', function() {
       out: 'main.min.js'
   })
   .pipe(uglify())
-  .pipe(gulp.dest(DIST + '/js'))
-  .pipe(notify('JS Build Complete'));
+  .pipe(gulp.dest(DIST + '/js'));
 });
 
-// Gulp Watcher
-gulp.task('watch', function() {
-  gulp.watch(SRC + '/styles/**/*.scss', ['scss']);
-  gulp.watch(SRC + '/js/**/*.js', ['jsBuild']);
+
+
+// Clean dist directory for rebuild
+gulp.task('clean', function() {
+  return gulp.src(DIST, {read: false})
+    .pipe(clean());
 });
+
+
+
+// Do the creep, ahhhhhhh!
+gulp.task('watch', function() {
+
+  // Listen on port 35729
+  server.listen(35729, function (err) {
+    if (err) {
+      return console.log(err)
+    }
+      
+    // Watch .scss files
+    gulp.watch(SRC + '/styles/**/*.scss', ['scss']);
+
+    // Watch image files
+    gulp.watch( SRC + '/images/**/*', ['compress-images']);
+
+  });
+
+});
+
  
 // Gulp Default Task
 gulp.task('default', ['scss', 'watch']);
 gulp.task('jsBuild', ['requirejsBuild']);
+gulp.task('image-min', ['compress-images']);
